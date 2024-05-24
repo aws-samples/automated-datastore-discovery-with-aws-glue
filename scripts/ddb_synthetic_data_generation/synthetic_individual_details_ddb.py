bdbf8a89111c91fbee932bccf7674af564047403
Tanveer Gupta <gupttan@amazon.co.uk>
2024-05-24 16:59:26 +0100

    add scripts and synthetic data stuff

---
+++ scripts/ddb_synthetic_data_generation/synthetic_individual_details_ddb.py
@@ -0,0 +1,198 @@
+import argparse
+import logging
+import os
+import time
+import uuid
+
+import boto3
+from boto3.dynamodb.types import TypeSerializer
+from botocore.exceptions import ClientError
+from faker import Faker
+
+
+DEFAULT_LOG_LEVEL = logging.INFO
+LOGGER = logging.getLogger(__name__)
+LOGGING_FORMAT = "%(asctime)s %(levelname)-5.5s " \
+                 "[%(name)s]:[%(threadName)s] " \
+                 "%(message)s"
+
+AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY"
+AWS_SESSION_TOKEN = "AWS_SESSION_TOKEN"
+
+TABLE_NAME = "IndividualDetails"
+
+MAX_RECORDS = 1000
+
+
+def _check_missing_field(validation_dict, extraction_key):
+    """Check if a field exists in a dictionary
+
+    :param validation_dict: Dictionary
+    :param extraction_key: String
+
+    :raises: Exception
+    """
+    extracted_value = validation_dict.get(extraction_key)
+    
+    if extracted_value is None:
+        LOGGER.error(f"Missing '{extraction_key}' key in the dict")
+        raise Exception
+    
+
+def _validate_field(validation_dict, extraction_key, expected_value):
+    """Validate the passed in field
+
+    :param validation_dict: Dictionary
+    :param extraction_key: String
+    :param expected_value: String
+
+    :raises: ValueError
+    """
+    extracted_value = validation_dict.get(extraction_key)
+    _check_missing_field(validation_dict, extraction_key)
+    
+    if extracted_value != expected_value:
+        LOGGER.error(f"Incorrect value found for '{extraction_key}' key")
+        raise ValueError
+
+
+def _cli_args():
+    """Parse CLI Args
+    
+    :rtype: argparse.Namespace
+    """
+    parser = argparse.ArgumentParser(description="synthetic-data-gen-ddb")
+    parser.add_argument("-e",
+                        "--env",
+                        action="store_true",
+                        help="Use environment variables for AWS credentials")
+    parser.add_argument("-p",
+                        "--aws-profile",
+                        type=str,
+                        default="default",
+                        help="AWS profile to be used for the API calls")
+    parser.add_argument("-r",
+                        "--aws-region",
+                        type=str,
+                        help="AWS region for API calls")
+    parser.add_argument("-m",
+                        "--max-records",
+                        type=int,
+                        default=MAX_RECORDS,
+                        help="Maximum records to be inserted")
+    parser.add_argument("-v",
+                        "--verbose",
+                        action="store_true",
+                        help="debug log output")
+    return parser.parse_args()
+
+
+def _silence_noisy_loggers():
+    """Silence chatty libraries for better logging"""
+    for logger in ['boto3', 'botocore',
+                   'botocore.vendored.requests.packages.urllib3']:
+        logging.getLogger(logger).setLevel(logging.WARNING)
+
+
+def main():
+    """What executes when the script is run"""
+    start = time.time() # to capture elapsed time
+
+    args = _cli_args()
+
+    # logging configuration
+    log_level = DEFAULT_LOG_LEVEL
+    if args.verbose:
+        log_level = logging.DEBUG
+    logging.basicConfig(level=log_level, format=LOGGING_FORMAT)
+    # silence chatty libraries
+    _silence_noisy_loggers()
+
+    if args.env:
+        LOGGER.info(
+            "Attempting to fetch AWS credentials via environment variables")
+        aws_access_key_id = os.environ.get(AWS_ACCESS_KEY_ID)
+        aws_secret_access_key = os.environ.get(AWS_SECRET_ACCESS_KEY)
+        aws_session_token = os.environ.get(AWS_SESSION_TOKEN)
+        if not aws_secret_access_key or not aws_access_key_id or not aws_session_token:
+            raise Exception(
+                f"Missing one or more environment variables - " 
+                f"'{AWS_ACCESS_KEY_ID}', '{AWS_SECRET_ACCESS_KEY}', "
+                f"'{AWS_SESSION_TOKEN}'"
+            )
+    else:
+        LOGGER.info(f"AWS Profile being used: {args.aws_profile}")
+        boto3.setup_default_session(profile_name=args.aws_profile)
+
+    region_check = [
+        # explicit cli argument
+        args.aws_region,
+        # check if set through ENV vars
+        os.environ.get('AWS_REGION'),
+        os.environ.get('AWS_DEFAULT_REGION'),
+        # else check if set in config or in boto already
+        boto3.DEFAULT_SESSION.region_name if boto3.DEFAULT_SESSION else None,
+        boto3.Session().region_name,
+    ]
+    aws_region = None
+    for aws_region in region_check:
+        if aws_region:
+            LOGGER.info(f"AWS Region: {aws_region}")
+            break
+    if not aws_region:
+        raise Exception("Need to have a valid AWS Region")
+
+    ddb_client = boto3.client("dynamodb")
+    table_name = os.environ.get(
+        "INDIVIDUAL_TABLE_NAME", TABLE_NAME)
+    # Check if table exists
+    list_table_resp = ddb_client.list_tables()
+    _check_missing_field(list_table_resp, "ResponseMetadata")
+    _validate_field(
+        list_table_resp["ResponseMetadata"], 
+        "HTTPStatusCode", 
+        200)
+    _check_missing_field(list_table_resp, "TableNames")
+    
+    tables = list_table_resp["TableNames"]
+    if table_name not in tables:
+        raise Exception(f"Table {table_name} does not exist")
+
+    serializer = TypeSerializer()
+    fake = Faker()
+    # Generate and insert data into the table
+    for _ in range(MAX_RECORDS):
+        python_obj = {
+        'id': str(uuid.uuid4()),
+        'Individual_Gender': fake.random_element(elements=('Male', 'Female')),
+        'Individual_Location': fake.city(),
+    }
+        try:
+            resp = ddb_client.put_item(
+                TableName=table_name,
+                Item={
+                    k: serializer.serialize(v) for k, v in python_obj.items()
+                    },
+                ConditionExpression="attribute_not_exists(id)",
+            )
+            _check_missing_field(resp, "ResponseMetadata")
+            _validate_field(resp["ResponseMetadata"], "HTTPStatusCode", 200)       
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
+                LOGGER.error(f"Primary Key violation")
+            else:
+                LOGGER.error("Unable to insert data into DynamoDB")
+                raise Exception
+
+        LOGGER.info(f"Inserted item with id: {python_obj['id']}.")
+
+    LOGGER.info(f"Data insertion complete for table: {table_name}.")
+
+    ddb_client.close()
+    LOGGER.info(f"Total time elapsed: {time.time() - start} seconds")
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ scripts/glue/ddb-source/ddb-source-script.py
@@ -0,0 +1,349 @@
+import sys
+import uuid
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglueml.transforms import EntityDetector
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.sql.types import ArrayType, StringType, StructType, StructField
+from pyspark.sql.functions import lit, current_timestamp, udf
+
+
+EXTRACTION_PARAMS = [
+    "JOB_NAME",
+    "ddbTableName",
+    "dataCatalogDatabase",
+    "dataCatalogTable",
+    "ddbTableID",
+    "region",
+    "outputTable",
+    "ddbTableARN",
+]
+
+ALL_ENTITIES = [
+        "PERSON_NAME",
+        "EMAIL",
+        "CREDIT_CARD",
+        "IP_ADDRESS",
+        "MAC_ADDRESS",
+        "PHONE_NUMBER",
+        "USA_ATIN",
+        "USA_PASSPORT_NUMBER",
+        "USA_PTIN",
+        "USA_SSN",
+        "USA_ITIN",
+        "BANK_ACCOUNT",
+        "USA_DRIVING_LICENSE",
+        "USA_HCPCS_CODE",
+        "USA_NATIONAL_DRUG_CODE",
+        "USA_NATIONAL_PROVIDER_IDENTIFIER",
+        "USA_DEA_NUMBER",
+        "USA_HEALTH_INSURANCE_CLAIM_NUMBER",
+        "USA_MEDICARE_BENEFICIARY_IDENTIFIER",
+        "JAPAN_BANK_ACCOUNT",
+        "JAPAN_DRIVING_LICENSE",
+        "JAPAN_MY_NUMBER",
+        "JAPAN_PASSPORT_NUMBER",
+        "UK_BANK_ACCOUNT",
+        "UK_BANK_SORT_CODE",
+        "UK_DRIVING_LICENSE",
+        "UK_ELECTORAL_ROLL_NUMBER",
+        "UK_NATIONAL_HEALTH_SERVICE_NUMBER",
+        "UK_NATIONAL_INSURANCE_NUMBER",
+        "UK_PASSPORT_NUMBER",
+        "UK_PHONE_NUMBER",
+        "UK_UNIQUE_TAXPAYER_REFERENCE_NUMBER",
+        "UK_VALUE_ADDED_TAX",
+        "ARGENTINA_TAX_IDENTIFICATION_NUMBER",
+        "AUSTRALIA_BUSINESS_NUMBER",
+        "AUSTRALIA_COMPANY_NUMBER",
+        "AUSTRALIA_DRIVING_LICENSE",
+        "AUSTRALIA_MEDICARE_NUMBER",
+        "AUSTRALIA_PASSPORT_NUMBER",
+        "AUSTRALIA_TAX_FILE_NUMBER",
+        "AUSTRIA_DRIVING_LICENSE",
+        "AUSTRIA_PASSPORT_NUMBER",
+        "AUSTRIA_SSN",
+        "AUSTRIA_TAX_IDENTIFICATION_NUMBER",
+        "AUSTRIA_VALUE_ADDED_TAX",
+        "BOSNIA_UNIQUE_MASTER_CITIZEN_NUMBER",
+        "KOSOVO_UNIQUE_MASTER_CITIZEN_NUMBER",
+        "MACEDONIA_UNIQUE_MASTER_CITIZEN_NUMBER",
+        "MONTENEGRO_UNIQUE_MASTER_CITIZEN_NUMBER",
+        "VOJVODINA_UNIQUE_MASTER_CITIZEN_NUMBER",
+        "SERBIA_UNIQUE_MASTER_CITIZEN_NUMBER",
+        "SERBIA_VALUE_ADDED_TAX",
+        "BELGIUM_DRIVING_LICENSE",
+        "BELGIUM_NATIONAL_IDENTIFICATION_NUMBER",
+        "BELGIUM_PASSPORT_NUMBER",
+        "BELGIUM_TAX_IDENTIFICATION_NUMBER",
+        "BELGIUM_VALUE_ADDED_TAX",
+        "BRAZIL_BANK_ACCOUNT",
+        "BRAZIL_NATIONAL_IDENTIFICATION_NUMBER",
+        "BRAZIL_NATIONAL_REGISTRY_OF_LEGAL_ENTITIES_NUMBER",
+        "BRAZIL_NATURAL_PERSON_REGISTRY_NUMBER",
+        "BULGARIA_DRIVING_LICENSE",
+        "BULGARIA_UNIFORM_CIVIL_NUMBER",
+        "BULGARIA_VALUE_ADDED_TAX",
+        "CANADA_DRIVING_LICENSE",
+        "CANADA_GOVERNMENT_IDENTIFICATION_CARD_NUMBER",
+        "CANADA_PASSPORT_NUMBER",
+        "CANADA_PERMANENT_RESIDENCE_NUMBER",
+        "CANADA_PERSONAL_HEALTH_NUMBER",
+        "CANADA_SOCIAL_INSURANCE_NUMBER",
+        "CHILE_DRIVING_LICENSE",
+        "CHILE_NATIONAL_IDENTIFICATION_NUMBER",
+        "CHINA_IDENTIFICATION",
+        "CHINA_LICENSE_PLATE_NUMBER",
+        "CHINA_MAINLAND_TRAVEL_PERMIT_ID_HONG_KONG_MACAU",
+        "CHINA_MAINLAND_TRAVEL_PERMIT_ID_TAIWAN",
+        "CHINA_PASSPORT_NUMBER",
+        "CHINA_PHONE_NUMBER",
+        "HONG_KONG_IDENTITY_CARD",
+        "MACAU_RESIDENT_IDENTITY_CARD",
+        "TAIWAN_NATIONAL_IDENTIFICATION_NUMBER",
+        "TAIWAN_PASSPORT_NUMBER",
+        "COLOMBIA_PERSONAL_IDENTIFICATION_NUMBER",
+        "COLOMBIA_TAX_IDENTIFICATION_NUMBER",
+        "CROATIA_DRIVING_LICENSE",
+        "CROATIA_IDENTITY_NUMBER",
+        "CROATIA_PASSPORT_NUMBER",
+        "CROATIA_PERSONAL_IDENTIFICATION_NUMBER",
+        "CYPRUS_DRIVING_LICENSE",
+        "CYPRUS_NATIONAL_IDENTIFICATION_NUMBER",
+        "CYPRUS_PASSPORT_NUMBER",
+        "CYPRUS_TAX_IDENTIFICATION_NUMBER",
+        "CYPRUS_VALUE_ADDED_TAX",
+        "CZECHIA_DRIVING_LICENSE",
+        "CZECHIA_PERSONAL_IDENTIFICATION_NUMBER",
+        "CZECHIA_VALUE_ADDED_TAX",
+        "DENMARK_DRIVING_LICENSE",
+        "DENMARK_PERSONAL_IDENTIFICATION_NUMBER",
+        "DENMARK_TAX_IDENTIFICATION_NUMBER",
+        "DENMARK_VALUE_ADDED_TAX",
+        "ESTONIA_DRIVING_LICENSE",
+        "ESTONIA_PASSPORT_NUMBER",
+        "ESTONIA_PERSONAL_IDENTIFICATION_CODE",
+        "ESTONIA_VALUE_ADDED_TAX",
+        "FINLAND_DRIVING_LICENSE",
+        "FINLAND_HEALTH_INSURANCE_NUMBER",
+        "FINLAND_NATIONAL_IDENTIFICATION_NUMBER",
+        "FINLAND_PASSPORT_NUMBER",
+        "FINLAND_VALUE_ADDED_TAX",
+        "FRANCE_BANK_ACCOUNT",
+        "FRANCE_DRIVING_LICENSE",
+        "FRANCE_HEALTH_INSURANCE_NUMBER",
+        "FRANCE_INSEE_CODE",
+        "FRANCE_NATIONAL_IDENTIFICATION_NUMBER",
+        "FRANCE_PASSPORT_NUMBER",
+        "FRANCE_TAX_IDENTIFICATION_NUMBER",
+        "FRANCE_VALUE_ADDED_TAX",
+        "GERMANY_BANK_ACCOUNT",
+        "GERMANY_DRIVING_LICENSE",
+        "GERMANY_PASSPORT_NUMBER",
+        "GERMANY_PERSONAL_IDENTIFICATION_NUMBER",
+        "GERMANY_TAX_IDENTIFICATION_NUMBER",
+        "GERMANY_VALUE_ADDED_TAX",
+        "GREECE_DRIVING_LICENSE",
+        "GREECE_PASSPORT_NUMBER",
+        "GREECE_SSN",
+        "GREECE_TAX_IDENTIFICATION_NUMBER",
+        "GREECE_VALUE_ADDED_TAX",
+        "HUNGARY_DRIVING_LICENSE",
+        "HUNGARY_PASSPORT_NUMBER",
+        "HUNGARY_SSN",
+        "HUNGARY_TAX_IDENTIFICATION_NUMBER",
+        "HUNGARY_VALUE_ADDED_TAX",
+        "ICELAND_NATIONAL_IDENTIFICATION_NUMBER",
+        "ICELAND_PASSPORT_NUMBER",
+        "ICELAND_VALUE_ADDED_TAX",
+        "INDIA_AADHAAR_NUMBER",
+        "INDIA_PERMANENT_ACCOUNT_NUMBER",
+        "INDONESIA_IDENTITY_CARD_NUMBER",
+        "IRELAND_DRIVING_LICENSE",
+        "IRELAND_PASSPORT_NUMBER",
+        "IRELAND_PERSONAL_PUBLIC_SERVICE_NUMBER",
+        "IRELAND_TAX_IDENTIFICATION_NUMBER",
+        "IRELAND_VALUE_ADDED_TAX",
+        "ISRAEL_IDENTIFICATION_NUMBER",
+        "ITALY_BANK_ACCOUNT",
+        "ITALY_DRIVING_LICENSE",
+        "ITALY_FISCAL_CODE",
+        "ITALY_PASSPORT_NUMBER",
+        "ITALY_VALUE_ADDED_TAX",
+        "KOREA_PASSPORT_NUMBER",
+        "KOREA_RESIDENCE_REGISTRATION_NUMBER_FOR_CITIZENS",
+        "KOREA_RESIDENCE_REGISTRATION_NUMBER_FOR_FOREIGNERS",
+        "LATVIA_DRIVING_LICENSE",
+        "LATVIA_PASSPORT_NUMBER",
+        "LATVIA_PERSONAL_IDENTIFICATION_NUMBER",
+        "LATVIA_VALUE_ADDED_TAX",
+        "LIECHTENSTEIN_NATIONAL_IDENTIFICATION_NUMBER",
+        "LIECHTENSTEIN_PASSPORT_NUMBER",
+        "LIECHTENSTEIN_TAX_IDENTIFICATION_NUMBER",
+        "LITHUANIA_DRIVING_LICENSE",
+        "LITHUANIA_PERSONAL_IDENTIFICATION_NUMBER",
+        "LITHUANIA_TAX_IDENTIFICATION_NUMBER",
+        "LITHUANIA_VALUE_ADDED_TAX",
+        "LUXEMBOURG_DRIVING_LICENSE",
+        "LUXEMBOURG_NATIONAL_INDIVIDUAL_NUMBER",
+        "LUXEMBOURG_PASSPORT_NUMBER",
+        "LUXEMBOURG_TAX_IDENTIFICATION_NUMBER",
+        "LUXEMBOURG_VALUE_ADDED_TAX",
+        "MALAYSIA_MYKAD_NUMBER",
+        "MALAYSIA_PASSPORT_NUMBER",
+        "MALTA_DRIVING_LICENSE",
+        "MALTA_NATIONAL_IDENTIFICATION_NUMBER",
+        "MALTA_TAX_IDENTIFICATION_NUMBER",
+        "MALTA_VALUE_ADDED_TAX",
+        "MEXICO_CLABE_NUMBER",
+        "MEXICO_DRIVING_LICENSE",
+        "MEXICO_PASSPORT_NUMBER",
+        "MEXICO_TAX_IDENTIFICATION_NUMBER",
+        "MEXICO_UNIQUE_POPULATION_REGISTRY_CODE",
+        "NETHERLANDS_BANK_ACCOUNT",
+        "NETHERLANDS_CITIZEN_SERVICE_NUMBER",
+        "NETHERLANDS_DRIVING_LICENSE",
+        "NETHERLANDS_PASSPORT_NUMBER",
+        "NETHERLANDS_TAX_IDENTIFICATION_NUMBER",
+        "NETHERLANDS_VALUE_ADDED_TAX",
+        "NEW_ZEALAND_DRIVING_LICENSE",
+        "NEW_ZEALAND_NATIONAL_HEALTH_INDEX_NUMBER",
+        "NEW_ZEALAND_TAX_IDENTIFICATION_NUMBER",
+        "NORWAY_BIRTH_NUMBER",
+        "NORWAY_DRIVING_LICENSE",
+        "NORWAY_HEALTH_INSURANCE_NUMBER",
+        "NORWAY_NATIONAL_IDENTIFICATION_NUMBER",
+        "NORWAY_VALUE_ADDED_TAX",
+        "PHILIPPINES_DRIVING_LICENSE",
+        "PHILIPPINES_PASSPORT_NUMBER",
+        "POLAND_DRIVING_LICENSE",
+        "POLAND_IDENTIFICATION_NUMBER",
+        "POLAND_PASSPORT_NUMBER",
+        "POLAND_REGON_NUMBER",
+        "POLAND_SSN",
+        "POLAND_TAX_IDENTIFICATION_NUMBER",
+        "POLAND_VALUE_ADDED_TAX",
+        "PORTUGAL_DRIVING_LICENSE",
+        "PORTUGAL_NATIONAL_IDENTIFICATION_NUMBER",
+        "PORTUGAL_PASSPORT_NUMBER",
+        "PORTUGAL_TAX_IDENTIFICATION_NUMBER",
+        "PORTUGAL_VALUE_ADDED_TAX",
+        "ROMANIA_DRIVING_LICENSE",
+        "ROMANIA_NUMERICAL_PERSONAL_CODE",
+        "ROMANIA_PASSPORT_NUMBER",
+        "ROMANIA_VALUE_ADDED_TAX",
+        "SINGAPORE_DRIVING_LICENSE",
+        "SINGAPORE_NATIONAL_REGISTRY_IDENTIFICATION_NUMBER",
+        "SINGAPORE_PASSPORT_NUMBER",
+        "SINGAPORE_UNIQUE_ENTITY_NUMBER",
+        "SLOVAKIA_DRIVING_LICENSE",
+        "SLOVAKIA_NATIONAL_IDENTIFICATION_NUMBER",
+        "SLOVAKIA_PASSPORT_NUMBER",
+        "SLOVAKIA_VALUE_ADDED_TAX",
+        "SLOVENIA_DRIVING_LICENSE",
+        "SLOVENIA_PASSPORT_NUMBER",
+        "SLOVENIA_TAX_IDENTIFICATION_NUMBER",
+        "SLOVENIA_UNIQUE_MASTER_CITIZEN_NUMBER",
+        "SLOVENIA_VALUE_ADDED_TAX",
+        "SOUTH_AFRICA_PERSONAL_IDENTIFICATION_NUMBER",
+        "SPAIN_BANK_ACCOUNT",
+        "SPAIN_DNI",
+        "SPAIN_DRIVING_LICENSE",
+        "SPAIN_NIE",
+        "SPAIN_NIF",
+        "SPAIN_PASSPORT_NUMBER",
+        "SPAIN_SSN",
+        "SPAIN_VALUE_ADDED_TAX",
+        "SRI_LANKA_NATIONAL_IDENTIFICATION_NUMBER",
+        "SWEDEN_DRIVING_LICENSE",
+        "SWEDEN_PASSPORT_NUMBER",
+        "SWEDEN_PERSONAL_IDENTIFICATION_NUMBER",
+        "SWEDEN_TAX_IDENTIFICATION_NUMBER",
+        "SWEDEN_VALUE_ADDED_TAX",
+        "SWITZERLAND_AHV",
+        "SWITZERLAND_HEALTH_INSURANCE_NUMBER",
+        "SWITZERLAND_PASSPORT_NUMBER",
+        "SWITZERLAND_VALUE_ADDED_TAX",
+        "THAILAND_PASSPORT_NUMBER",
+        "THAILAND_PERSONAL_IDENTIFICATION_NUMBER",
+        "TURKEY_NATIONAL_IDENTIFICATION_NUMBER",
+        "TURKEY_PASSPORT_NUMBER",
+        "TURKEY_VALUE_ADDED_TAX",
+        "UKRAINE_INDIVIDUAL_IDENTIFICATION_NUMBER",
+        "UKRAINE_PASSPORT_NUMBER_DOMESTIC",
+        "UKRAINE_PASSPORT_NUMBER_INTERNATIONAL",
+        "UNITED_ARAB_EMIRATES_PERSONAL_NUMBER",
+        "VENEZUELA_DRIVING_LICENSE",
+        "VENEZUELA_NATIONAL_IDENTIFICATION_NUMBER",
+        "VENEZUELA_VALUE_ADDED_TAX"
+]
+
+args = getResolvedOptions(sys.argv, EXTRACTION_PARAMS)
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+print(args)
+
+entities = ALL_ENTITIES
+
+input_table = glueContext.create_dynamic_frame.from_catalog(
+    database=args["dataCatalogDatabase"],
+    table_name=args["dataCatalogTable"],
+    transformation_ctx="input_table",
+)
+
+# Script generated for node Detect Sensitive Data
+entity_detector = EntityDetector()
+classified_map = entity_detector.classify_columns(
+    input_table,
+    entities,
+    1.0,
+    0.1,
+)
+items = classified_map.items()
+schema = StructType(
+    [
+        StructField("columnName", StringType(), True),
+        StructField("entityTypes", ArrayType(StringType(), True)),
+    ]
+)
+data_frame = spark.createDataFrame(data=items, schema=schema)
+
+uuidUdf= udf(lambda : str(uuid.uuid4()),StringType())
+
+data_frame_inter = data_frame. \
+                    withColumn("id", uuidUdf()). \
+                    withColumn("timestamp", current_timestamp()). \
+                    withColumn("data_source_type", lit("dynamodb")). \
+                    withColumn("ddbTableID", lit(args["ddbTableID"])). \
+                    withColumn("ddbTableARN", lit(args["ddbTableARN"])). \
+                    withColumn("ddbTableName", lit(args["ddbTableName"])). \
+                    withColumn("data_catalog_table", lit(args["dataCatalogTable"])). \
+                    withColumn("data_catalog_database", lit(args["dataCatalogDatabase"])). \
+                    withColumn("region", lit(args["region"]))
+
+
+dd = DynamicFrame.fromDF(
+    data_frame_inter, glueContext, "df_for_pii"
+)
+
+dd.show()
+
+dd.printSchema()
+
+
+sink =  glueContext.write_dynamic_frame.from_options(
+    frame=dd,
+    connection_type="dynamodb",
+    connection_options={
+        "dynamodb.output.tableName": args["outputTable"],
+    }
+)
+
+job.commit()
